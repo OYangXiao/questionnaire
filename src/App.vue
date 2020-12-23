69096c5be01a344d032dcf076411788a1a65242e
ouyangxiao <oyangxiao@outlook.com>
2020-12-23 17:55:20 +0800

    增加基本项目配置及代码

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import questionConfigs from './question-config';
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
+  name: 'App',
+  data() {
+    return {
+      questionConfigs,
+    };
+  },
 };
 </script>
 
---
+++ src/question-config.js
@@ -0,0 +1,8 @@
+export const questions = [
+  { title: 'aaa', options: ['甲', '乙', '丙', '丁'] },
+  { title: 'bbb', options: ['甲', '乙', '丙', '丁'] },
+  { title: 'ccc', options: ['甲', '乙', '丙', '丁'] },
+  { title: 'ddd', options: ['甲', '乙', '丙', '丁'] },
+  { title: 'eee', options: ['甲', '乙', '丙', '丁'] },
+  { title: 'fff', options: ['甲', '乙', '丙', '丁'] },
+];
